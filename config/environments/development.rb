9f2e781300b5d486b0ba9b124ba61c2e03ac87b0
yoshidablue <sugar.blue.1224@gmail.com>
2022-12-02 05:53:09 +0000

    グループメンバーにメール機能

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,5 +75,19 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+    config.action_mailer.default_url_options = {host: 'localhost', post: 3000}
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      port:                 587,
+      address:              'smtp.gmail.com',
+      domain:               'gmail.com',
+      user_name:            ENV['KEY'],
+      password:             ENV['SECRET_KEY'],
+      authentication:       'login',
+      enable_starttls_auto: true
+    }
+
   config.hosts << "80bd8f6dcf78415eafd1eb312268bcc6.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
